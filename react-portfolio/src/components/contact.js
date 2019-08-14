b3dfdfa37be8b3ed62fe9777988920d9766476a3
jvassas <jakevassas@gmail.com>
2019-08-14 19:31:37 -0400

    initial form

--- react-portfolio/src/components/contact.js
+++ react-portfolio/src/components/contact.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Form, Button } from 'react-bootstrap';
 
-    class Contact extends Component {
-        render(){
-            return (
-                <div><h1>Contact Page</h1></div>
-            )
-        }
-    }
+class Contact extends Component {
+  render() {
+    return (
+    <div className="contact">
+      <Form>
+          <h1>Contact</h1>
+        <Form.Group controlId="name-input" className="name-input">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Name" />
+        </Form.Group>
+        <Form.Group controlId="email-input" className="email-input">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="message-input" className="message-input">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows="5" placeholder="Your message" />
+        </Form.Group>
+        <Button variant="secondary" className="submit">Submit</Button>
+      </Form>
+    </div>
+    );
+  }
+}
 
-    export default Contact;
+export default Contact;
