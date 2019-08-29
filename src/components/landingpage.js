9d72e8d1cabb56520671b445431973587867abf2
jvassas <jakevassas@gmail.com>
2019-08-29 18:59:23 -0400

    new font

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -16,19 +16,18 @@ class Landing extends Component {
           <Card>
             <Card.Img variant="top" className="code" src={Code}/>
             <Card.Body>
-              <Card.Title><h3>Certified Full Stack Developer</h3></Card.Title>
+              <Card.Title><h3>Full Stack Developer</h3></Card.Title>
               <Card.Text>
                 <p> I was presented a Certificate of Full-Stack Web Development from Case Western University in July 2019. The program helped me become skilled in both Front and Back portions of web applications. </p>
                 <p> My knowledge of front-end technologies consists of HTML5, CSS3, UI frameworks such as Bootstrap and Materialize, Javascript, jQuery, ES6, AJAX and React.js.</p> 
                 <p> My understanding of back-end technologies is composed of the Command Line, Node.js, Express.js, MySQL, MongoDB and Java. I also possess deployment skills using Heroku and Git.  </p>
-                
               </Card.Text>
             </Card.Body>
           </Card>
           <Card>
             <Card.Img variant="top" className="health" src={Health} />
             <Card.Body>
-              <Card.Title><h3>Bachelors of Science in Health Sciences</h3></Card.Title>
+              <Card.Title><h3>Bachelors of Science</h3></Card.Title>
               <Card.Text>
                 <p>I received my Bachelors of Science from Cleveland State University in December of 2018. During my time at CSU, I gained self-directed and engaged learning skills necessary for a lifetime of learning. </p>
                 <p>The program included a great core knowledge of cultural and ethical considerations not only specific to healthcare, but rather everyday life. I was also taught the basics of healthcare in the United States such as how a health system operates, insurance practices and communication within the medical field. </p>
