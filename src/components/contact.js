32c7026bc80282d0d343c4991c989b8f44d7d6c1
jvassas <jakevassas@gmail.com>
2019-09-30 14:53:55 -0400

    contact form, some styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
-//  Bootstrap and axios imports for contact form 
+//  Bootstrap and axios imports for contact form
 // import { Form, Button } from 'react-bootstrap';
 // import axios from 'axios';
 
 class Contact extends Component {
-    render() {
-        return (
-            <div className="contact-info"><h1>Contact</h1>
-                <h4>Send me an <a href="mailto:jakevassas@gmail.com?Subject=Hello%20Jake" target="_top">email</a></h4>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="contact-info">
+        <div className="row">
+          <div className="col-12">
+          <h1>Contact</h1>
+            <form
+              action=" https://formsubmit.co/37afd3813b004f392654463be0398b28"
+              method="POST" id="contact-form"
+            >
+              Name: <br />
+              <input type="text" name="name" id="form-name"/> <br />
+              Email: <br />
+              <input type="email" name="email" id="form-email" /> <br />
+              Message: <br />
+              <textarea name="message" id="form-message"></textarea> <br />
+              <button type="submit" id="form-button">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-   
-   
 
 export default Contact;
 
-
+{
+  /* <h4>Send me an <a href="mailto:jakevassas@gmail.com?Subject=Hello%20Jake" target="_top">email</a></h4> */
+}
 // Contact form that sends email (unsure if I want this)
 
 // constructor() {
@@ -48,7 +64,6 @@ export default Contact;
 //     })
 // }
 
-
 // render() {
 // return (
 // <div className="contact">
@@ -56,16 +71,16 @@ export default Contact;
 //       <h1>Contact</h1>
 //     <Form.Group controlId="name-input" className="name-input">
 //       <Form.Label for="name">Name</Form.Label>
-//       <Form.Control 
-//         type="text" 
+//       <Form.Control
+//         type="text"
 //         name="name"
-//         placeholder="Name" 
+//         placeholder="Name"
 //         onChange={this.handleChange}/>
 //     </Form.Group>
 //     <Form.Group controlId="email-input" className="email-input">
 //       <Form.Label for="email">Email</Form.Label>
-//       <Form.Control 
-//         type="email" 
+//       <Form.Control
+//         type="email"
 //         name="email"
 //         placeholder="name@example.com"
 //         onChange={this.handleChange} />
@@ -74,7 +89,7 @@ export default Contact;
 //       <Form.Label for="message">Message</Form.Label>
 //       <Form.Control
 //         as="textarea"
-//         name="message" 
+//         name="message"
 //         rows="5" placeholder="Your message"
 //         onChange={this.handleChange} />
 //     </Form.Group>
